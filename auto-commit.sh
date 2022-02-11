cbf03ff3ad9da8aad8282f5aabf7101a9ab1de51
Okdenn <okkuphy@gmail.com>
2022-02-11 16:49:42 +0900

    2022年 2月11日 金曜日 16時49分42秒 JST

--- auto-commit.sh
+++ auto-commit.sh
@@ -5,8 +5,8 @@ do
     git reset
     git add auto-commit.sh
     git add graph/.
-    git add okuda-abstract/.
-    git add okuda-master-thesis/.
+    git add okuda-abstract-再提出/.
+    git add okuda-master-thesis-再提出/.
     git commit -m "`date`"
     git push origin master
     sleep 900
